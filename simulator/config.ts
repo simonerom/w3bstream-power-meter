d0498728eeef649ac5b178015703ff983a47dac8
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-04-18 17:32:23 +0200

    Create config.ts

---
+++ simulator/config.ts
@@ -0,0 +1,6 @@
+export default {
+    PUB_ID: "energy_pub",
+    PUB_TOKEN:
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJQYXlsb2FkIjoiOTAyMzc0MjU4NTA1NjI2MiIsImlzcyI6InczYnN0cmVhbSJ9.caIkr5IzY1oGT9SQJUJrqjJeyM8SC_O9pcCX6P9Yplg",
+    W3BSTREAM_ENDPOINT: "https://dev.w3bstream.com/api/w3bapp/event/energy_test",
+}
