b471df4ab68634dfa81d17bdd0b33b7750c29ed8
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-04-18 17:43:12 +0200

    use env file for vars

--- simulator/config.ts
+++ simulator/config.ts
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export default {
-    PUB_ID: "energy_pub",
-    PUB_TOKEN:
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJQYXlsb2FkIjoiOTAyMzc0MjU4NTA1NjI2MiIsImlzcyI6InczYnN0cmVhbSJ9.caIkr5IzY1oGT9SQJUJrqjJeyM8SC_O9pcCX6P9Yplg",
-    W3BSTREAM_ENDPOINT: "https://dev.w3bstream.com/api/w3bapp/event/energy_test",
-}
+  PUB_ID: process.env.PUB_ID || "",
+  PUB_TOKEN: process.env.PUB_TOKEN || "",
+  W3BSTREAM_ENDPOINT: `https://dev.w3bstream.com/api/w3bapp/event/${process.env.PROJECT_NAME}`,
+};
