adff0d80645e401c89615c701194b58aac854764
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-04-20 18:25:34 +0200

    Update index.ts

--- w3bstream-as/assembly/index.ts
+++ w3bstream-as/assembly/index.ts
@@ -4,6 +4,7 @@ export { alloc } from "@w3bstream/wasm-sdk";
 
 import { mintRewards } from "./rewards/mint-rewards";
 import { getField, getPayloadValue } from "./utils/payload-parser";
+import { validateMessage } from "./utils/message-validation";
 
 const RECIPIENT_ADDR = "0x36f075ef0437b5fe95a7d0293823f1e085416ddf";
 const TOKEN_CONTRACT_ADDR = "0x0da8d9FBb86120f74af886265c51b98B1BeAE395";
@@ -13,24 +14,20 @@ export function start(rid: i32): i32 {
   logOnCall();
 
   const message = getMessage(rid);
-  const payload = getPayloadValue(message);
-  const data = getField<JSON.Obj>(payload, "data");
+  validateMessage(message);
 
-  if (data == null) {
-    return 1;
-  }
+  // message verification
+  // message processing
 
-  const device_pub_key = getPublicKey(payload);
-  const reading = getField<JSON.Float>(data, "sensor_reading");
-  const timestamp = getField<JSON.Integer>(data, "timestamp");
-
-  if (validateMessage(device_pub_key, reading, timestamp) == 1) {
-    return 1;
-  }
-
-  evaluateConsumtionAndMintRewards(reading!);
+  const reading = getReading(message);
+  evaluateConsumtionAndMintRewards(reading);
 
   return 0;
+}
+
+function logOnCall(): void {
+  Log("start() called");
+  Log("TokenContractAddress: " + TOKEN_CONTRACT_ADDR);
 }
 
 function getMessage(rid: i32): string {
@@ -40,55 +37,12 @@ function getMessage(rid: i32): string {
   return deviceMessage;
 }
 
-function logOnCall(): void {
-  Log("start() called");
-  Log("TokenContractAddress: " + TOKEN_CONTRACT_ADDR);
-}
+function getReading(message: string): JSON.Float {
+  const payload = getPayloadValue(message);
+  const data = getField<JSON.Obj>(payload, "data");
+  const reading = getField<JSON.Float>(data!, "sensor_reading");
 
-function getPublicKey(payload: JSON.Obj): JSON.Str | null {
-  return payload.getString("public_key");
-}
-
-function validatePubKey(pubKey: JSON.Str | null): i32 {
-  if (pubKey == null) {
-    Log("Missing device public key, ignoring this data point.");
-    return 1;
-  }
-  let pubKeyValue: string = pubKey.valueOf();
-  Log("device_pub_key: " + pubKeyValue);
-
-  return 0;
-}
-
-function validateReading(reading: JSON.Float | null): i32 {
-  if (reading == null) {
-    Log("sensor reading is null, ignoring this data point.");
-    return 1;
-  }
-  return 0;
-}
-
-function validateTimestamp(timestamp: JSON.Integer | null): i32 {
-  if (timestamp == null) {
-    Log("timestamp is null, ignoring this data point.");
-    return 1;
-  }
-  return 0;
-}
-
-function validateMessage(
-  pubKey: JSON.Str | null,
-  reading: JSON.Float | null,
-  timestamp: JSON.Integer | null
-): i32 {
-  if (
-    validatePubKey(pubKey) == 1 ||
-    validateReading(reading) == 1 ||
-    validateTimestamp(timestamp) == 1
-  ) {
-    return 1;
-  }
-  return 0;
+  return reading!;
 }
 
 function evaluateConsumtionAndMintRewards(reading: JSON.Float): void {
